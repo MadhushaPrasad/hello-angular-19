90d25dfd08c3d92dcd6ea5845603803e25169851
Madhusha Prasad <madushaprasad21@gmail.com>
2025-05-09 17:06:09 +0530

    chore: customer save function done

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
@@ -16,24 +16,34 @@ export class CustomerComponent {
   email: string = ""
   password: string = ""
 
-  customers:any[]=[
-    {id:"c001", firstName:"Madhusha",lastName:"Prasad",email:"madushaprasad21@gmail.com",password:"123456"},
-    {id:"c002", firstName:"Kasun",lastName:"silva",email:"madushaprasad21@gmail.com",password:"123456"},
-    {id:"c003", firstName:"Nimal",lastName:"perera",email:"madushaprasad2321@gmail.com",password:"123456"},
-    {id:"c004", firstName:"kaml",lastName:"fernando",email:"madushaprasad3221@gmail.com",password:"123456"},
-    {id:"c005", firstName:"danum",lastName:"lakshan",email:"madushap32rasad21@gmail.com",password:"123456"},
+  customers: any[] = [
+    { id: "c001", firstName: "Madhusha", lastName: "Prasad", email: "madushaprasad21@gmail.com", password: "123456" },
+    { id: "c002", firstName: "Kasun", lastName: "silva", email: "madushaprasad21@gmail.com", password: "123456" },
+    { id: "c003", firstName: "Nimal", lastName: "perera", email: "madushaprasad2321@gmail.com", password: "123456" },
+    { id: "c004", firstName: "kaml", lastName: "fernando", email: "madushaprasad3221@gmail.com", password: "123456" },
+    { id: "c005", firstName: "danum", lastName: "lakshan", email: "madushap32rasad21@gmail.com", password: "123456" },
   ]
 
 
   submitForm() {
-   console.log(this.firstName)
-   console.log(this.lastName)
-   console.log(this.email)
-   console.log(this.password)
+    console.log(this.firstName)
+    console.log(this.lastName)
+    console.log(this.email)
+    console.log(this.password)
   }
 
 
-  saveCustomer(){
+  saveCustomer() {
+
+    const row = {
+      id: "c" + Math.floor(Math.random() * 1000),
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password
+    }
+
+    this.customers.push(row)
 
   }
 
