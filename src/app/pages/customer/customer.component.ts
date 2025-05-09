5f966e378cb4cb72419b46218bffc5ab8d60427e
Madhusha Prasad <madushaprasad21@gmail.com>
2025-05-09 17:01:42 +0530

    chore: display customer details on table

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
@@ -15,12 +16,25 @@ export class CustomerComponent {
   email: string = ""
   password: string = ""
 
+  customers:any[]=[
+    {id:"c001", firstName:"Madhusha",lastName:"Prasad",email:"madushaprasad21@gmail.com",password:"123456"},
+    {id:"c002", firstName:"Kasun",lastName:"silva",email:"madushaprasad21@gmail.com",password:"123456"},
+    {id:"c003", firstName:"Nimal",lastName:"perera",email:"madushaprasad2321@gmail.com",password:"123456"},
+    {id:"c004", firstName:"kaml",lastName:"fernando",email:"madushaprasad3221@gmail.com",password:"123456"},
+    {id:"c005", firstName:"danum",lastName:"lakshan",email:"madushap32rasad21@gmail.com",password:"123456"},
+  ]
+
 
   submitForm() {
    console.log(this.firstName)
    console.log(this.lastName)
    console.log(this.email)
    console.log(this.password)
+  }
+
+
+  saveCustomer(){
+
   }
 
 
