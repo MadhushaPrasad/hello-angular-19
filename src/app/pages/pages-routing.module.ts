02acb0360a39672924c5d35365c649848bcf7b51
Madhusha Prasad <madushaprasad21@gmail.com>
2025-05-09 16:01:46 +0530

    fix: fixed pages routes

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,33 +4,38 @@ import { PagesComponent } from './pages.component';
 import { CustomerComponent } from './customer/customer.component';
 import { ItemsComponent } from './items/items.component';
 import { OrdersComponent } from './orders/orders.component';
+import { AuthGuard } from '../guard/auth-guard.service';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch:"full",
-    redirectTo:"customer",
+    pathMatch: 'full',
+    redirectTo: 'customer'
+  },
+  {
+    path: '',
     component: PagesComponent,
     children: [
       {
-        path:'customer/:id/:name',
-        component:CustomerComponent
-      },
-       {
-        path:'customer/:id',
-        component:CustomerComponent
-      },
-            {
-        path:'customer',
-        component:CustomerComponent
+        path: 'customer/:id/:name',
+        component: CustomerComponent
       },
       {
-        path:'items',
-        component:ItemsComponent
+        path: 'customer/:id',
+        component: CustomerComponent
       },
       {
-        path:'orders',
-        component:OrdersComponent
+        path: 'customer',
+        component: CustomerComponent
+      },
+      {
+        path: 'items',
+        component: ItemsComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'orders',
+        component: OrdersComponent
       }
     ]
   }
