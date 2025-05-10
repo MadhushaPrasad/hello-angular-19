c7c16cdffa6a8674c6433a7f37fcd07ec67696a7
Madhusha Prasad <madushaprasad21@gmail.com>
2025-05-10 17:17:49 +0530

    feat: create crud services for post

--- src/app/post/post.service.ts
+++ src/app/post/post.service.ts
@@ -8,9 +8,25 @@ import { Post } from '../dto/post';
 })
 export class PostService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAllPosts():Observable<Post[]>{
+  getAllPosts(): Observable<Post[]> {
     return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+  }
+
+  savePost(post: Post): Observable<boolean> {
+    return this.http.post<boolean>('https://jsonplaceholder.typicode.com/posts', post)
+  }
+
+  updatePost(post: Post): Observable<boolean> {
+    return this.http.put<boolean>('https://jsonplaceholder.typicode.com/posts/' + post.id, post)
+  }
+
+  deletePost(id: string): Observable<boolean> {
+    return this.http.delete<boolean>('https://jsonplaceholder.typicode.com/posts/' + id)
+  }
+
+  getPostById(id: number): Observable<Post> {
+    return this.http.get<Post>('https://jsonplaceholder.typicode.com/posts/1')
   }
 }
